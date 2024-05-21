fe173900ac072e07a49d31cb93389c52be376d37
Andrey Kozichev <akozichev@gmail.com>
2024-05-21 22:34:19 +0100

    added the first report view

---
+++ models/reports/driver_daily.sql
@@ -0,0 +1,17 @@
+SELECT
+    u.firstname || ' ' || u.lastname AS driver_name,
+    COUNT(*) AS delivery_count,
+    SUM(t.mileage) AS total_km_driven,
+    SUM(t.totalcost) AS total_daily_cost,
+    SUM(CASE 
+                WHEN ps.urgency ILIKE '%Hotshot%' OR ps.urgency ILIKE '%Hot shot%' 
+                THEN t.totalcost * 0.79 
+                ELSE t.totalcost * 0.69 
+              END) AS total_days_compensation,
+    (t.WhenDroppedOff AT TIME ZONE 'UTC' AT TIME ZONE 'MST')::date AS entry_date_mst
+FROM {{ ref('ontime_tracking') }} AS t
+INNER JOIN {{ ref('ontime_users') }} AS u ON t.droppedOffByDriverID = u.ID
+INNER JOIN {{ ref('ontime_pricesets') }} AS ps ON t.PriceSet = ps.ID
+GROUP BY driver_name, (t.WhenDroppedOff AT TIME ZONE 'UTC' AT TIME ZONE 'MST')::date
+ORDER BY
+    entry_date_mst
