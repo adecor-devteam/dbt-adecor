79bf4145a9d0fbd3ceda12e30cd8784ccb89e987
Andrey Kozichev <akozichev@gmail.com>
2024-05-23 19:42:33 +0100

    one more model

--- models/reports/driver_daily_total_summary_report.sql
+++ models/reports/driver_daily_total_summary_report.sql
@@ -1,13 +1,13 @@
 SELECT
-    date_trunc('day', i.WhenDroppedOff AT TIME ZONE 'UTC' AT TIME ZONE 'MST') AS "Date",
-    COUNT(*) AS "Total Delivery Count",
-    round(SUM(i.Mileage), 2) AS "Total KM's Driven",
-    SUM(i.TotalCost)::text::money AS "Total Cost",
+    date_trunc('day', i.WhenDroppedOff AT TIME ZONE 'UTC' AT TIME ZONE 'MST') AS "date",
+    COUNT(*) AS "total_delivery_count",
+    round(SUM(i.Mileage), 2) AS "total_km_driven",
+    SUM(i.TotalCost)::text::money AS "total_cost",
     SUM(CASE
                 WHEN ps.Urgency LIKE '%Hotshot%' OR ps.Urgency LIKE '%Hot shot%'
                 THEN i.TotalCost * 0.79
                 ELSE i.TotalCost * 0.69
-              END)::text::money AS "Total Compensation",
+              END)::text::money AS "total_compensation",
     (SUM(i.TotalCost) - SUM(CASE
                 WHEN ps.Urgency LIKE '%Hotshot%' OR ps.Urgency LIKE '%Hot shot%'
                 THEN i.TotalCost * 0.79
@@ -17,4 +17,4 @@ FROM {{ ref('ontime_tracking') }} AS i
 INNER JOIN {{ ref('ontime_users') }} AS u ON i.DroppedOffByDriverID = u.ID
 INNER JOIN {{ ref('ontime_pricesets') }} AS ps ON i.PriceSet = ps.ID
 WHERE u.FirstName ~ '^[0-9][0-9] '
-GROUP BY 1;
+GROUP BY 1
