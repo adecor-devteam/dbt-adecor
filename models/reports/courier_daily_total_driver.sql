f71f4117feb3f6e398a66ce4d4e90b2dc38c68da
Andrey Kozichev <akozichev@gmail.com>
2024-05-26 12:02:01 +0100

    fixed dependencies on models

--- models/reports/courier_daily_total_driver.sql
+++ models/reports/courier_daily_total_driver.sql
@@ -10,8 +10,8 @@ SELECT
                 ELSE t.totalcost * 0.69  
               END), 'FM$999999999.00') AS total_compensation,
     (t.whendroppedoff AT TIME ZONE 'UTC' AT TIME ZONE 'MST')::date AS entry_date_mst
-FROM ontime_tracking AS t
-INNER JOIN ontime_users AS u ON t.droppedoffbydriverid = u.id
-INNER JOIN ontime_pricesets AS ps ON t.priceset = ps.id
+FROM {{ ref('ontime_tracking') }} AS t
+INNER JOIN {{ ref('ontime_users') }} AS u ON t.droppedoffbydriverid = u.id
+INNER JOIN {{ ref('ontime_pricesets') }} AS ps ON t.priceset = ps.id
 WHERE u.firstname ~ '^(0[1-9]|[1-9][0-9]|1[0-9]{2}|2[0-9]{2}) '
 GROUP BY u.firstname, u.lastname, u.id, (t.whendroppedoff AT TIME ZONE 'UTC' AT TIME ZONE 'MST')::date
--- models/reports/courier_daily_total_summary.sql
+++ models/reports/courier_daily_total_summary.sql
@@ -11,10 +11,10 @@ SELECT
                 WHEN ps.urgency ILIKE '%Hotshot%' OR ps.urgency ILIKE '%Hot shot%' 
                 THEN t.totalcost * 0.79 
                 ELSE t.totalcost * 0.69 
-                END), 'FM$999999999.00') AS "net",
+                END), 'FM$999999999.00') AS net,
     (t.whendroppedoff AT TIME ZONE 'UTC' AT TIME ZONE 'MST')::date AS entry_date_mst
-FROM ontime_tracking AS t
-INNER JOIN ontime_users AS u ON t.droppedoffbydriverid = u.id
-INNER JOIN ontime_pricesets AS ps ON t.priceset = ps.id
+FROM {{ ref('ontime_tracking') }} AS t
+INNER JOIN {{ ref('ontime_users') }} AS u ON t.droppedoffbydriverid = u.id
+INNER JOIN {{ ref('ontime_pricesets') }} AS ps ON t.priceset = ps.id
 WHERE u.firstname ~ '^(0[1-9]|[1-9][0-9]|1[0-9]{2}|2[0-9]{2}) '
 GROUP BY (t.whendroppedoff AT TIME ZONE 'UTC' AT TIME ZONE 'MST')::date
