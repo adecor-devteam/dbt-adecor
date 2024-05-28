20af674787c9a93b6318fb2a86aa177b3aaa9f43
Andrey Kozichev <akozichev@gmail.com>
2024-05-28 18:25:01 +0100

    commas in numbers

--- models/reports/trucking_daily_total_driver.sql
+++ models/reports/trucking_daily_total_driver.sql
@@ -2,13 +2,13 @@ SELECT
     u.firstname || ' ' || u.lastname AS driver_name,
     LOWER(u.id) as driver_id,
     TO_CHAR(COUNT(*), 'FM999,999,999') AS total_delivery_count,
-    TO_CHAR(ROUND(SUM(t.mileage),2), 'FM999999999.00') AS total_km_driven,
-    TO_CHAR(ROUND(SUM(t.totalcost),2), 'FM$999999999.00') AS total_cost,
+    TO_CHAR(ROUND(SUM(t.mileage),2), 'FM999,999,999.00') AS total_km_driven,
+    TO_CHAR(ROUND(SUM(t.totalcost),2), 'FM$999,999,999.00') AS total_cost,
     TO_CHAR(ROUND(SUM(CASE 
                 WHEN ps.urgency ILIKE '%Hotshot%' OR ps.urgency ILIKE '%Hot shot%' 
                 THEN ROUND(t.totalcost * 0.79,2)  
                 ELSE ROUND(t.totalcost * 0.69,2)  
-              END),2), 'FM$999999999.00') AS total_compensation,
+              END),2), 'FM$999,999,999.00') AS total_compensation,
     (t.whendroppedoff AT TIME ZONE 'UTC' AT TIME ZONE 'MST')::date AS entry_date_mst,
     TO_CHAR((t.whendroppedoff AT TIME ZONE 'UTC' AT TIME ZONE 'MST'), 'Month DD, YYYY') AS delivery_date
 FROM {{ ref('ontime_tracking') }} AS t
