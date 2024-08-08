d6a171fba8026141186e0eec8c10018cbfe786a3
Andrey Kozichev <akozichev@gmail.com>
2024-08-08 20:40:02 +0100

    updated data calculation

--- models/website/www_invoices.sql
+++ models/website/www_invoices.sql
@@ -3,7 +3,7 @@ SELECT
     iit.ontime_invoice_date as invoice_date,
     iit.qb_invoice_created_at as creation_date,
     iit.company as company_name,
-    iit.orders_total - ROUND((iit.orders_total - iit.orders_total * iit.discount) * iit.gst / 100,2) as ontime_total,
+    ROUND((iit.orders_total - iit.orders_total * iit.discount/100),2) + ROUND(ROUND((iit.orders_total - iit.orders_total * iit.discount/100),2) * iit.gst / 100,2) as ontime_total,
     iit.quickbooks_total as quickbooks_total,
     iit.qb_invoice_status as processed_in_qb,
     CASE    
