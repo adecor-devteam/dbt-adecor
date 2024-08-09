852cfbb9b159a58d9532f73880118a89fcf14316
Andrey Kozichev <akozichev@gmail.com>
2024-08-09 12:01:11 +0100

    distinct invoices and extract emails

--- models/website/interim_invoice_total.sql
+++ models/website/interim_invoice_total.sql
@@ -1,4 +1,5 @@
-SELECT 	oi.invoicenumber as invoicenumber,
+SELECT DISTINCT ON	(oi.invoicenumber)
+		oi.invoicenumber as invoicenumber,
         oi.invoicedate as ontime_invoice_date,
         it.qb_invoice_created_at as qb_invoice_created_at,
         oc.company as company,
@@ -6,6 +7,9 @@ SELECT 	oi.invoicenumber as invoicenumber,
 		ROUND(qi.total_amount,2) as quickbooks_total,
 		EXTRACT(DAY FROM NOW() - it.qb_invoice_created_at) AS days_since_creation,
 		qi.balance as quickbooks_balance,
+		iet.pdf_sendgrid_messageid as sendgrid_messageid,
+		aeaf.status,
+		aeaf.email,
 		COALESCE(cd.gst, 5.0) AS gst,
 		COALESCE(cd.discount, 0) AS discount,
         sum(ROUND(ot.totalcost,2)) AS orders_total
@@ -15,4 +19,23 @@ from  {{ ref('ontime_invoices_orders') }}  oi
 		left join {{ ref('ontime_tracking') }} ot on (oi.tracking_id = ot.id )
 		left join {{ source('arms','customer_details')}} cd on (cd.ontime_customerid = oi.customerid)
 		left join {{ ref('quickbooks_invoice') }} qi on ( oi.invoicenumber::TEXT = qi.doc_number)
-group by oi.invoicenumber, oi.invoicedate, it.qb_invoice_created_at, oc.company, cd.gst, cd.discount, it.qb_invoice_status, qi.total_amount, qi.balance
+		left join invoice_email_tracking iet on ( iet.invoiceid = oi.invoicenumber )
+		left join arms_email_activity_feed aeaf on  (aeaf.batch_message_id = iet.pdf_sendgrid_messageid)
+group by 	oi.invoicenumber, 
+			oi.invoicedate, 
+			it.qb_invoice_created_at, 
+			oc.company, 
+			cd.gst, 
+			cd.discount, 
+			it.qb_invoice_status, 
+			qi.total_amount, 
+			qi.balance, 
+			iet.pdf_sendgrid_messageid,
+			aeaf.status,
+			aeaf.email,
+			iet.id,
+			aeaf.creation_date
+order by 
+			oi.invoicenumber, 
+			iet.id desc, 
+			aeaf.creation_date desc
