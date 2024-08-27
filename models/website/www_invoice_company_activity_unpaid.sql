45bdd29f9b5fb7c0fbd4ecd81d9d8bf6bad5218e
Andrey Kozichev <akozichev@gmail.com>
2024-08-27 20:13:55 +0100

    included views for unpaid invoices

---
+++ models/website/www_invoice_company_activity_unpaid.sql
@@ -0,0 +1,36 @@
+-- Invoice Number - (ontime_invoices)
+-- Payment Status - (quickbooks_invoice) If Balance = 0 then Paid, Otherwise Unpaid
+-- Emails Sent - (invoice_tracking)
+-- Invoice Status - (quickbooks_invoice) If Balance > 0 then Calculate days from Created Date (i.e. 5 days)
+-- Invoice Amount - (invoice_tracking)
+-- Rewards - (invoice_tracking) If Invoice is Paid and Rewards in (customer_details) = True Calculates total rewards from Subtotal * 0.025 and then rounds down to nearest ($40). In short the rewards is awarded 1 mile for every $40.
+-- Discount - (customer_details)
+-- GST - (customer_details)
+-- Convenience Fee - (customer_details)
+-- Total Owing - (quickbooks_invoice)
+-- Last Updated - This is the last time the invoice was processed so I believe you can use the qb_invoice_created_at column
+
+SELECT
+	iit.invoicenumber as invoice_number,
+    iit.company as Company,
+    CASE    
+        WHEN iit.quickbooks_balance > 0 THEN 'Unpaid'
+        ELSE 'Paid'
+    END AS payment_status,
+    iit.delivered_email_count as emails_sent,
+    case 
+    	when iit.quickbooks_balance > 0 then iit.days_since_creation
+    	else 0
+    end as invoice_status,
+    ROUND((iit.orders_total - iit.orders_total * iit.discount/100),2) + ROUND(ROUND((iit.orders_total - iit.orders_total * iit.discount/100),2) * iit.gst / 100,2) as invoice_amount,
+    case 
+    	when (iit.quickbooks_balance = 0 and iit.reward_air_miles = true) then TRUNC((ROUND((iit.orders_total - iit.orders_total * iit.discount/100),2) + ROUND(ROUND((iit.orders_total - iit.orders_total * iit.discount/100),2) * iit.gst / 100,2)) * 0.025)
+    	else 0
+    end as rewards,
+    iit.discount as discount,
+    iit.gst as gst,
+    iit.convenience_fee as convenience_fee,
+    iit.quickbooks_balance as total_owing,
+    iit.qb_invoice_created_at as last_updated
+FROM {{ ref('interim_invoice_total') }} iit
+WHERE iit.quickbooks_balance > 0
---
+++ models/website/www_invoices_unpaid.sql
@@ -0,0 +1,16 @@
+SELECT
+    iit.invoicenumber as invoice_number,
+    iit.ontime_invoice_date as invoice_date,
+    iit.ontime_invoice_date::TEXT as created_date_text,
+    iit.qb_invoice_created_at as creation_date,
+    iit.company as company_name,
+    ROUND((iit.orders_total - iit.orders_total * iit.discount/100),2) + ROUND(ROUND((iit.orders_total - iit.orders_total * iit.discount/100),2) * iit.gst / 100,2) as ontime_total,
+    iit.quickbooks_total as quickbooks_total,
+    iit.qb_invoice_status as processed_in_qb,
+    CASE    
+        WHEN iit.quickbooks_balance > 0 THEN iit.days_since_creation
+        ELSE 0
+    END AS invoice_status,
+    iit.delivered_email_count as email_status
+FROM {{ ref('interim_invoice_total') }} iit
+WHERE iit.quickbooks_balance > 0
